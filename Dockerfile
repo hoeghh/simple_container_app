f6bc6beb062ad1374175ea081ee9794269d8416e
Henrik René Høegh <heh@praqma.net>
2017-01-25 16:22:25 +0100

    restructure of folders

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:latest
 MAINTAINER Henrik Hoegh "heh@praqma.net"
 RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
-COPY . /app
+COPY /app /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
---
+++ scripts/jenkins-dsl/pipeline.dsl.groovy
@@ -0,0 +1,83 @@
+// FreeStyle job examples
+// This script needs Docker installed on the machine running the job.
+
+folder("Telenor-Pipeline")
+
+job("Telenor-Pipeline/BuildAndPush") {
+  scm {
+      git {
+          remote {
+              name('Simple-Container-App')
+              url('https://github.com/hoeghh/simple_container_app.git')
+          }
+          extensions {
+              cleanAfterCheckout()
+              relativeTargetDirectory('simple-container-app')
+          }
+      }
+  }
+
+  triggers {
+    scm('H/15 * * * *')
+  }
+
+  //Shell step
+  steps {
+		shell("echo 'Hi Henrik', checking out... \$GIT_COMMIT")
+	}
+  steps {
+		shell("./simple-container-app/script/build.sh \$GIT_COMMIT")
+	}
+  steps {
+		shell("./simple-container-app/script/push.sh \$GIT_COMMIT")
+	}
+
+  steps {
+  	//Automatisk
+  	downstreamParameterized {
+  		trigger("deploy_to_stagging") {
+  			parameters {
+                  gitRevision()
+        }
+  		}
+  	}
+  }
+}
+
+
+job("Telenor-Pipeline/deploy_to_stagging") {
+  steps {
+    shell("./simple-container-app/script/stag-deploy.sh \$GIT_COMMIT")
+  }
+
+  steps {
+    //Automatisk
+  	downstreamParameterized {
+  		trigger("simple_test") {
+  			parameters {
+                  gitRevision()
+        }
+  		}
+  	}
+  }
+}
+
+job("Telenor-Pipeline/simple_test") {
+  steps {
+    shell("./simple-container-app/script/test.sh \$GIT_COMMIT")
+  }
+}
+
+
+job("Telenor-Pipeline/deploy_to_production") {
+  steps {
+    shell("./simple-container-app/script/prod_deploy.sh \$GIT_COMMIT")
+  }
+}
+
+job("Telenor-Pipeline/rollback-production") {
+  steps {
+    shell("./simple-container-app/script/prod_rollback.sh \$GIT_COMMIT")
+  }
+}
+
