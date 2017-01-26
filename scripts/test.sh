713dbf9efe32b894ff962d85b030bb3c71ba9d75
Henrik René Høegh <heh@praqma.net>
2017-01-26 13:05:21 +0100

    better test with response code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,9 @@
-curl -k --header "Host: $1.example.com" https://lb.example.com
+
+responsecode=$(curl -s -k -I --header "Host: $1.example.com" https://lb.example.com | head -n 1|cut -d$' ' -f2)
+if [ $responsecode == "200" ]; then
+  echo "worked: $responsecode"
+  exit 0
+else
+  echo "failed: $responsecode"
+  exit 1
+fi
