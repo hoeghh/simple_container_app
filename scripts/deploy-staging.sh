907dfc9ef33b3d6717b54c5f768e938fa34d78ff
Henrik René Høegh <heh@praqma.net>
2017-01-26 11:00:00 +0100

    Generating yaml for kubernetes deploy to staging

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,13 +1,58 @@
 echo "Deploying app to staging."
 kubectl create namespace $1
-kubectl create -f deployment/dep-my-app.yaml --namespace=$1
-kubectl create -f deployment/srv-my-app.yaml --namespace=$1
+
+echo "
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: myapp
+  namespace: $1
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: myapp
+  template:
+    metadata:
+      name: myapp
+      labels:
+        app: myapp
+        run: myapp
+    spec:
+      containers:
+      - name: myapp
+        image: hoeghh/myapp:$1
+        ports:
+        - name: web
+          containerPort: 5000
+" > deployment/dep-my-app.yaml
+
+echo "
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    name: myapp
+    app: myapp
+  name: myapp
+  namespace: $1
+spec:
+  selector:
+    app: myapp
+  type: ClusterIP
+  ports:
+  - name: myapp
+    protocol: TCP
+    port: 5000
+
+"
 
 echo "
 apiVersion: extensions/v1beta1
 kind: Ingress
 metadata:
   name: myapp
+  namespace: $1
 spec:
   rules:
   - host: $1.example.com
@@ -19,4 +64,7 @@ spec:
           servicePort: 5000
 
 " > deployment/ing-my-app.yaml
+
+kubectl create -f deployment/dep-my-app.yaml --namespace=$1
+kubectl create -f deployment/srv-my-app.yaml --namespace=$1
 kubectl create -f deployment/ing-my-app.yaml --namespace=$1
