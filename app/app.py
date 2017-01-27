c7aab58a7c4153facdcdea590b8d27e7032bbebd
Henrik René Høegh <heh@praqma.net>
2017-01-27 13:43:07 +0100

    Added Oslo

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return 'Hello world from Praqma in Aarhus.'
+        return 'Hello world from Praqma in Aarhus and Oslo'
 
 if __name__ == '__main__':
         app.run(debug=True,host='0.0.0.0')
