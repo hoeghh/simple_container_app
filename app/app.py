2199c4346e0e36af1b386ad1a5e65a710ffa6580
Henrik René Høegh <heh@praqma.net>
2017-02-06 15:06:13 +0100

    nice production app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return 'Hello world from Praqma in :<br>Aarhus and Oslo and Odense. What a great day.... <b>AAARGGG...crash...</b>'
+        return 'Hello world from Praqma in :<br>Aarhus and Oslo and Odense. What a great day.... <b></b>'
 
 if __name__ == '__main__':
         app.run(debug=True,host='0.0.0.0')
