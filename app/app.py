b41cd17564b22cf0bc6bdf3d9df6834115bdba60
Henrik René Høegh <heh@praqma.net>
2017-02-07 11:16:33 +0100

    Added Telenor 1 try

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return 'Hello world from Praqma in :<br>Aarhus and Oslo and Odense. What a great day.... <b></b>'
+        return 'Hello world from Praqma in :<br>Aarhus and Oslo and Odense. What a great day.... <b>Telenor #¤S</b>'
 
 if __name__ == '__main__':
         app.run(debug=True,host='0.0.0.0')
