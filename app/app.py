354bb07a26a9bc76c3569c27d9124418497f584f
Henrik René Høegh <heh@praqma.net>
2017-08-29 19:41:33 +0200

    Added Stibo

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return 'Hello all. We love everyone ! '
+        return 'Hello Stibo. We love everyone ! '
 
 if __name__ == '__main__':
         app.run(debug=True,host='0.0.0.0')
