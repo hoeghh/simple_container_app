11780ae5a4a3d0b5653177ec79437505803fd5d5
Henrik René Høegh <heh@praqma.net>
2017-10-10 09:16:27 +0200

    Changed url for kubernetes ingress controller

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 
-responsecode=$(curl -s -k -I --header "Host: $1.example.com" https://controlplane.kubernetes | head -n 1|cut -d$' ' -f2)
+responsecode="$(curl -s -k -I --header "Host: $1.example.com" https://kubemaster.kubernetes | head -n 1|cut -d$' ' -f2)"
 if [ $responsecode == "200" ]; then
   echo "worked: $responsecode"
   exit 0
